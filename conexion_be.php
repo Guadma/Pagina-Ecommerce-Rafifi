131550aff598d40f0e21909408a375cc22d50a0d
Guadma <151107324+Guadma@users.noreply.github.com>
2024-01-03 08:22:32 -0300

    Add files via upload

---
+++ conexion_be.php
@@ -0,0 +1,11 @@
+<?php
+
+    $conexion = mysqli_connect("localhost","root","","login_register");
+
+    // if ($conexion) {
+    //     echo 'Conexion exitosa a la base de datos';
+    // }else {
+    //     echo 'No se logro conexion a la base de datos';
+    // }
+
+?>
